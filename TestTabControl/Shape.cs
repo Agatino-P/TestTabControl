a54c5fd5581cfc391bcf4354d8f233f6a598c758
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-05-03 03:06:14 +0200

    Used a StyleTrigger to change the contenttemplate of ContentControl, both in DataGrid Details and in standalone ContentControl

--- TestTabControl/Shape.cs
+++ TestTabControl/Shape.cs
@@ -8,7 +8,9 @@ namespace TestTabControl
 {
     abstract public class Shape : ObservableObject
     {
-        private  string _name; public string Name { get => _name; set { Set(() => Name, ref _name, value); }}
+        private string _name; 
+        public string Name { get => _name; set { Set(() => Name, ref _name, value); }}
+
 
         private double _w; public double W { get => _w; set { Set(() => W, ref _w, value); } }
         private double _h; public double H { get => _h; set { Set(() => H, ref _h, value); } }
